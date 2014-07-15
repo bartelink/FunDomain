1e95fa1245dc8c8719805c02484633458b6bfee8
bartelink <ruben@bartelink.com>
2014-07-15 01:27:27 +0100

    Improved whitespace consistency

--- Samples/FunUno.Tests/When playing a second turn.fs
+++ Samples/FunUno.Tests/When playing a second turn.fs
@@ -8,28 +8,28 @@ open System
 
 let context = [   
     GameStarted {GameId=1; PlayerCount=4; FirstCard=Digit(3, Red)}
-    CardPlayed  {GameId=1; Player=0; Card=Digit(9, Red) } ]
+    CardPlayed  {GameId=1; Player=0; Card=Digit(9, Red)} ]
 
 let [<Fact>] ``Same color should be accepted`` () =
     <@ replay context
-    |> handle ( PlayCard {GameId=1; Player=1; Card=Digit(8, Red) } ) 
+    |> handle ( PlayCard {GameId=1; Player=1; Card=Digit(8, Red)} ) 
     =[ CardPlayed {GameId=1; Player=1; Card=Digit(8, Red)} ] @> 
     |> test
 
 let [<Fact>] ``Same value should be accepted`` () =
     <@ replay context
-    |> handle ( PlayCard {GameId=1; Player=1; Card=Digit(9, Yellow) } ) 
+    |> handle ( PlayCard {GameId=1; Player=1; Card=Digit(9, Yellow)} ) 
     =[ CardPlayed {GameId=1; Player=1; Card=Digit(9, Yellow)} ] @> 
     |> test
 
 let [<Fact>] ``Different value and color should be rejected`` () =
     <@ replay context
-    |> handle ( PlayCard {GameId=1; Player=1; Card=Digit(8, Yellow) } ) @>
+    |> handle ( PlayCard {GameId=1; Player=1; Card=Digit(8, Yellow)} ) @>
     |> raises<InvalidOperationException>
 
 let [<Fact>] ``First player should play at his turn``() =
     <@ replay context
-    |> handle ( PlayCard {GameId=1; Player=4; Card=Digit(9, Green) } ) @>
+    |> handle ( PlayCard {GameId=1; Player=4; Card=Digit(9, Green)} ) @>
     |> raises<InvalidOperationException>
 
 let [<Fact>] ``After a full round it should be player 0 turn``() =
@@ -39,5 +39,5 @@ let [<Fact>] ``After a full round it should be player 0 turn``() =
                 CardPlayed {GameId=1; Player=2; Card=Digit(6, Red)}
                 CardPlayed {GameId=1; Player=3; Card=Digit(6, Red)} ] 
     |> handle ( PlayCard {GameId=1; Player=0; Card=Digit(1, Red)} )                
-    =[ CardPlayed {GameId=1; Player=0; Card=Digit(1, Red) } ] @> 
+    =[ CardPlayed {GameId=1; Player=0; Card=Digit(1, Red)} ] @> 
     |> test
--- Samples/FunUno.Tests/When starting game.fs
+++ Samples/FunUno.Tests/When starting game.fs
@@ -8,12 +8,13 @@ open System
 
 let [<Fact>] ``Started game should be started`` () = 
     <@ replay [ ] 
-    |> handle ( StartGame {GameId=1; PlayerCount=3; FirstCard=Digit(3, Red) })
-    =[ GameStarted { GameId=1; PlayerCount=3; FirstCard=Digit(3, Red)} ] @> |> test
+    |> handle ( StartGame {GameId=1; PlayerCount=3; FirstCard=Digit(3, Red)})
+    =[ GameStarted {GameId=1; PlayerCount=3; FirstCard=Digit(3, Red)} ] @> 
+    |> test
 
 let [<Fact>] ``0 players should be rejected`` () = 
     <@ replay [ ]
-    |> handle ( StartGame {GameId=1; PlayerCount=0; FirstCard=Digit(3, Red)} ) @>
+    |> handle ( StartGame {GameId=1; PlayerCount=0; FirstCard=Digit(3, Red)}) @>
     |> raises<ArgumentException> 
 
 let [<Fact>] ``Game should not be started twice`` () =
