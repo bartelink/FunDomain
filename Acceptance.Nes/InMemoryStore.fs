405a4e26e1224321917ca39a683a801cf232b2d9
bartelink <ruben@bartelink.com>
2014-07-17 11:30:52 +0100

    NEventStore linkage cleanup renamed NesStore -> NesGateway code cleanup in impl

--- Acceptance.Nes/InMemoryStore.fs
+++ Acceptance.Nes/InMemoryStore.fs
@@ -4,29 +4,30 @@ open FunUno
 open FunUno.UnoGame
 open FunUno.UnoGame.Events
 
-open FunDomain
-open FunDomain.Persistence
+open FunDomain // CommandHandler
+open FunDomain.Persistence.NEventStore // NesStore
 
 open Xunit
 open System
 
-let [<Fact>] ``Full Game with NES using InMemoryPersistence`` () =
-    let store = NesStore.createInMemory()
+let fullGameActions gameId = [
+    StartGame { GameId=gameId; PlayerCount=4; TopCard=Digit(3, Red) }
+    PlayCard  { GameId=gameId; Player=0; Card=Digit(3, Blue) }
+    PlayCard  { GameId=gameId; Player=1; Card=Digit(8, Blue) }
+    PlayCard  { GameId=gameId; Player=2; Card=Digit(8, Yellow) }
+    PlayCard  { GameId=gameId; Player=3; Card=Digit(4, Yellow) }
+    PlayCard  { GameId=gameId; Player=0; Card=Digit(4, Green) } ]
 
+let [<Fact>] ``Can run a full round using NEventStore's InMemoryPersistence`` () =
     let gameId = 42
 
     let unoGameHandler = CommandHandler.create UnoGame.replay UnoGame.handle 
 
-    let actions = [
-        StartGame { GameId=gameId; PlayerCount=4; TopCard=Digit(3, Red) }
-        PlayCard  { GameId=gameId; Player=0; Card=Digit(3, Blue) }
-        PlayCard  { GameId=gameId; Player=1; Card=Digit(8, Blue) }
-        PlayCard  { GameId=gameId; Player=2; Card=Digit(8, Yellow) }
-        PlayCard  { GameId=gameId; Player=3; Card=Digit(4, Yellow) }
-        PlayCard  { GameId=gameId; Player=0; Card=Digit(4, Green) } ]
+    let store = NesGateway.createInMemory()
 
-    let streamId = "_",string gameId
+    let bucket = "default"
+    let streamId = bucket,string gameId
 
-    for action in actions do 
+    for action in fullGameActions gameId do 
         printfn "%A" action
         action |> unoGameHandler store.read store.append streamId
--- FunDomain.Persistence.NEventStore/NesGateway.fs
+++ FunDomain.Persistence.NEventStore/NesGateway.fs
@@ -1,4 +1,4 @@
-﻿module FunDomain.Persistence.NesStore
+﻿module FunDomain.Persistence.NEventStore.NesGateway
 
 open FunDomain.Persistence.Serialization
 
@@ -9,18 +9,21 @@ open NEventStore.Persistence.Sql.SqlDialects
 open System
 open System.Collections.Generic
 
-type FunStreamer private (inner') = 
-    // Hoop jumping a la C++ pimpl pattern - if we don't do this, we're foisting an NEventStore package reference on all downstream users
-    let inner : IStoreEvents = unbox inner'
-    let get (bucketId,streamId) minRevision maxRevision =
-        inner.Advanced.GetFrom(bucketId, streamId, minRevision, maxRevision) 
-    let commit = inner.Advanced.Commit >> ignore
+/// Opaque token yielded by NesStreamer.read and consumed by NesStreamer.append
+type NesToken = { CommitSequence : int; StreamRevision : int}
 
+/// Wrapper class yielded by NesStore.create* functions with create/append functions matching FunDomain.CommandHandler requirements
+type NesStreamer private (inner') = 
+    // Hoop jumping a la C++ pimpl pattern - if we don't do this, we're foisting an NEventStore package reference on all downstream users
+    let inner : IPersistStreams = unbox inner'
+    let load (bucketId,streamId) minRevision maxRevision =
+        inner.GetFrom(bucketId, streamId, minRevision, maxRevision) 
+    let commit = inner.Commit >> ignore
     let readStream streamId startIndex count =
         let minRevision,maxRevision = startIndex,startIndex+count-1
         async {
             let commits = 
-                get streamId minRevision maxRevision
+                load streamId minRevision maxRevision
                 |> Array.ofSeq
             let events =
                 commits
@@ -31,18 +34,17 @@ type FunStreamer private (inner') =
                     deserializeUnion caseName blob)
                 |> Seq.choose id
                 |> List.ofSeq
-            let version = 
-                if commits.Length <> 0 then
+            let tokenOption = 
+                if commits.Length = 0 then 
+                    None 
+                else 
                     let lastCommit = commits |> Seq.last
-                    lastCommit.CommitSequence,lastCommit.StreamRevision
-                else
-                    1,1 // TODO verify
+                    Some {CommitSequence=lastCommit.CommitSequence; StreamRevision=lastCommit.StreamRevision}
 
-            return events, version, None }
+            return events, tokenOption, None }
 
-    let appendToStream (bucketId,streamId) streamMeta expectedVersion events =
+    let appendToStream (bucketId,streamId) streamMeta token events =
         let commitId,commitStamp,commitHeaders = streamMeta
-        let commitSequence,streamRevision = expectedVersion
         async {
             let eventMessages = 
                 events |> Seq.map (fun event ->
@@ -51,10 +53,20 @@ type FunStreamer private (inner') =
                     headers.["type"] <- box caseName
                     let body = box blob
                     EventMessage(Headers=headers, Body=body))
-            let attempt = CommitAttempt(bucketId,streamId,streamRevision+1,commitId,commitSequence+1,commitStamp,commitHeaders,eventMessages)
+            let updatedStreamRevision=token |> Option.map (fun token -> token.StreamRevision+1)
+            let updatedCommitSequence=token |> Option.map (fun token -> token.CommitSequence+1) 
+            let attempt = 
+                CommitAttempt(
+                    bucketId, streamId, 
+                    updatedStreamRevision |> defaultArg <| 1, 
+                    commitId, 
+                    updatedCommitSequence |> defaultArg <| 1, 
+                    commitStamp, 
+                    commitHeaders, 
+                    eventMessages)
             commit attempt}
 
-    static member internal wrap inner = FunStreamer( box inner)
+    static member internal wrap persister = NesStreamer( box persister)
 
     member this.read streamId = 
         let events,version,_ = 
@@ -62,25 +74,29 @@ type FunStreamer private (inner') =
             |> Async.RunSynchronously
         version,events
 
-    member this.append streamId expectedVersion events = 
+    member this.append streamId token events = 
         let commitMetadata() =
             let commitId = Guid.NewGuid() 
             let commitDateTime = DateTime.UtcNow
             let commitHeaders = null
             commitId,commitDateTime,commitHeaders
         let metadata = commitMetadata() 
-        appendToStream streamId metadata expectedVersion events
+        appendToStream streamId metadata token events
         |> Async.RunSynchronously
 
+let createFromStore (inner:IStoreEvents) = 
+    inner.Advanced |> NesStreamer.wrap
+
 let createInMemory () = 
-    FunStreamer.wrap <| Wireup.Init()
+    Wireup.Init()
         .LogToOutputWindow()
         .UsingInMemoryPersistence()
         .UsingJsonSerialization()
         .Build()
+    |> createFromStore
 
 let createInMsSqlWithPerfCounters (connectionString:string) perfCounterSetName = 
-    FunStreamer.wrap <| Wireup.Init()
+    Wireup.Init()
         .UsingSqlPersistence(connectionString)
         .WithDialect(new MsSqlDialect())
         .InitializeStorageEngine()
@@ -88,3 +104,4 @@ let createInMsSqlWithPerfCounters (connectionString:string) perfCounterSetName =
         .UsingJsonSerialization()
             .Compress()
         .Build()
+    |> createFromStore
