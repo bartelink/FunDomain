f1b439d4487a7527d6c9771749bc94e20b11c8a2
bartelink <ruben@bartelink.com>
2014-08-21 23:52:21 +0100

    Synced Serialization with FsUno.Prod - Updated Serialization to include FsUno.Prod's single case DU and struct support enhancements - Updated Json.net from 6.0.3 to 6.0.4

---
+++ FunDomain.Tests/SerializationFacts.fs
@@ -0,0 +1,50 @@
+﻿module FunDomain.Tests.SerializationFacts
+
+open FunDomain.Persistence.Serialization
+
+open Newtonsoft.Json
+open System.IO
+open Xunit
+open Swensen.Unquote
+
+let createSerializer converters =
+    let serializer = JsonSerializer()
+    converters |> List.iter serializer.Converters.Add
+    serializer
+
+let serialize converters o = 
+    let serializer = createSerializer converters
+    use w = new StringWriter()
+    serializer.Serialize(w, o)
+    w.ToString()
+
+let deserialize<'a> converters s =
+    let serializer = createSerializer converters
+    use r = new StringReader(s)
+    serializer.Deserialize<'a>(new JsonTextReader(r))
+
+module structs = 
+    type Digit = struct val value:int; new value = { value = value } end
+
+    let [<Fact>] ``a value should be serialized as its content`` () =
+        let serialize = serialize [ valueConverter typeof<Digit> ]
+
+        test <@ "7" = (Digit 7 |> serialize) @>
+
+    let [<Fact>] ``a value should be deserialized from its content`` () =
+        let deserialize = deserialize [ valueConverter typeof<Digit> ]
+    
+        test <@ Digit 7 = ("7" |> deserialize) @>
+
+module ``Single case DUs`` = 
+    type GameId = GameId of int
+
+    let [<Fact>] ``a single case union should be serialized as its content`` () =
+        let serialize = serialize [ unionConverter ]
+
+        test <@ "1234" = (GameId 1234 |> serialize) @>
+
+    let [<Fact>] ``a single case union should be deserialized from its content`` () =
+        let deserialize = deserialize [ unionConverter ]
+    
+        test <@ GameId 1234 = ("1234" |> deserialize)  @>
