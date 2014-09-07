1e8ad03334adc41674ff0c52f3b308ce014267ea
bartelink <ruben@bartelink.com>
2014-09-07 04:14:02 +0100

    Cleaned ComandHandler

--- FunDomain/CommandHandler.fs
+++ FunDomain/CommandHandler.fs
@@ -1,23 +1,21 @@
 ﻿module FunDomain.CommandHandler
 
-let inline create//<'state,'event,'command,'token,'streamId,'r
-//        when 'state : (static member evolve: 'state->('event->'state))
-//        and 'state : (static member initial: 'state)>
+let inline create
         (handle:'state->'command->('event list))
         (read:'streamId->int->int->Async<EncodedEvent seq*'token*int option>)
         (append:'streamId->'token->EncodedEvent list->Async<'r>) =
     let load stream =
         let rec fold state version = async {
             let sliceSize = 500
-            let! events, sliceLastToken, nextMinEventNumber = read stream version sliceSize
+            let! events, token, nextVersion = read stream version sliceSize
             let updatedPosition = 
                 events 
                 |> Seq.choose EncodedEvent.deserializeUnionByCaseItemTypeName<'event>
                 |> List.ofSeq
                 |> List.fold evolve' state
-            match nextMinEventNumber with
-            | None -> return sliceLastToken, updatedPosition
-            | Some minStreamIndex -> return! fold updatedPosition minStreamIndex }
+            match nextVersion with
+            | None -> return token, updatedPosition
+            | Some minVersion -> return! fold updatedPosition minVersion }
         let initialState = initial' ()
         fold initialState 0
 
