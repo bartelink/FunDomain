717bf072eb70715e85276858892f09c595f6e054
bartelink <ruben@bartelink.com>
2016-02-06 08:31:59 +0000

    Include missing build files

--- build.cmd
+++ build.cmd
@@ -8,9 +8,11 @@ if not exist .paket\paket.exe (
     )
 )
 
-.paket\paket.exe restore
-if errorlevel 1 (
-    exit /b %errorlevel%
+if not exist packages (
+    .paket\paket.exe restore
+    if errorlevel 1 (
+        exit /b %errorlevel%
+    )
 )
 
 packages\FAKE\tools\FAKE.exe build.fsx %*
