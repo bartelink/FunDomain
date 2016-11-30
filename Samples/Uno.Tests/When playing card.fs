a3002091ce4c11df6724677b9424d3514f0fb374
Ruben Bartelink <ruben@bartelink.com>
2016-11-30 10:36:37 +0000

    Reformat tests using professional experience

--- Samples/Uno.Tests/When playing card.fs
+++ Samples/Uno.Tests/When playing card.fs
@@ -8,35 +8,31 @@ open Swensen.Unquote
 open Xunit
 
 let gameId = GameId 1
-let context = [ GameStarted { GameId = gameId; PlayerCount = 4; FirstCard = red 3; FirstPlayer = 0 } ]
+let context = [ GameStarted { GameId = gameId; PlayerCount = 4; FirstCard = red 3; FirstPlayer = 0 }]
 
 let [<Fact>] ``Same color should be accepted``() = 
-    test <@ replay context
-        |> handle <| PlayCard { GameId = gameId; Player = 0; Card = red 9 } 
-        =[ CardPlayed { GameId = gameId; Player = 0; Card = red 9; NextPlayer = 1 } ] @>
+    test <@ context |> replay |> handle <| PlayCard { GameId = gameId; Player = 0; Card = red 9 }
+        = [ CardPlayed { GameId = gameId; Player = 0; Card = red 9; NextPlayer = 1 } ] @>
 
 let [<Fact>] ``Same value should be accepted``() =
-    test <@ replay context
-        |> handle <| PlayCard { GameId = gameId; Player = 0; Card = yellow 3 } 
-        =[ CardPlayed { GameId = gameId; Player = 0; Card = yellow 3; NextPlayer = 1 } ] @>
+    test <@ context |> replay|> handle <| PlayCard { GameId = gameId; Player = 0; Card = yellow 3 }
+        = [ CardPlayed { GameId = gameId; Player = 0; Card = yellow 3; NextPlayer = 1 } ] @>
 
 let [<Fact>] ``Different value and color should be rejected``() =
-    test <@ replay context
-        |> handle <| PlayCard { GameId = gameId; Player = 0; Card = yellow 8 } 
-        =[ PlayedWrongCard { GameId = gameId; Player = 0; Card = yellow 8}] @> 
+    test <@ context |> replay |> handle <| PlayCard { GameId = gameId; Player = 0; Card = yellow 8 }
+        = [ PlayedWrongCard { GameId = gameId; Player = 0; Card = yellow 8}] @> 
 
 let [<Fact>] ``First player should play at his turn``() =
-    test <@ replay context
-        |> handle <| PlayCard { GameId = gameId; Player = 2; Card = green 3 } 
-        =[ PlayedAtWrongTurn { GameId = gameId; Player = 2; Card = green 3 } ] @>
+    test <@ context |> replay |> handle <| PlayCard { GameId = gameId; Player = 2; Card = green 3 }
+        = [ PlayedAtWrongTurn { GameId = gameId; Player = 2; Card = green 3 } ] @>
     
 let [<Fact>] ``First player player after starting with kickback should be the dealer, next one should be on the right`` () =
-    test <@ replay [GameStarted { GameId = gameId; PlayerCount = 4; FirstCard = KickBack Green; FirstPlayer = 0 } 
-                    DirectionChanged { GameId = gameId; Direction = CounterClockWise }]
-        |> handle <| PlayCard { GameId = gameId; Player = 0; Card = green 3 } 
-        =[ CardPlayed { GameId = gameId; Player = 0; Card = green 3; NextPlayer = 3 }] @>
+    test <@ [   GameStarted { GameId = gameId; PlayerCount = 4; FirstCard = KickBack Green; FirstPlayer = 0 }
+                DirectionChanged { GameId = gameId; Direction = CounterClockWise } ]
+            |> replay |> handle <| PlayCard { GameId = gameId; Player = 0; Card = green 3 } 
+        = [ CardPlayed { GameId = gameId; Player = 0; Card = green 3; NextPlayer = 3 }] @>
 
 let [<Fact>] ``First player player after starting with skip should be the one after the dealer``() =
-    test <@ replay [ GameStarted { GameId = gameId; PlayerCount = 4; FirstCard = Skip Green; FirstPlayer = 1 } ]
-        |> handle <| PlayCard { GameId = gameId; Player = 1; Card = green 3 } 
-        =[ CardPlayed { GameId = gameId; Player = 1; Card = green 3; NextPlayer = 2 } ] @>
+    test <@ [ GameStarted { GameId = gameId; PlayerCount = 4; FirstCard = Skip Green; FirstPlayer = 1 } ]
+            |> replay |> handle <| PlayCard { GameId = gameId; Player = 1; Card = green 3 }
+        = [ CardPlayed { GameId = gameId; Player = 1; Card = green 3; NextPlayer = 2 } ] @>
